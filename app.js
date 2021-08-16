f361f28146e73f2a7e3da0a40b9ec52d561c610f
rutu14 <51828157+rutu14@users.noreply.github.com>
2021-08-16 18:38:44 +0530

    Api added,styling done.

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 var btnTranslate=document.querySelector("#btn-translate")
+var txtarea=document.querySelector("#txt")
+var outtxt=document.querySelector("#outtxt")
+
+url_server="https://api.funtranslations.com/translate/minion.json"
+function gettranslation(input){
+    return url_server+"?"+"text="+input
+}
+
 btnTranslate.addEventListener("click",function clickEventHandler(){
-    console.log("Clicked.")
+    var input=txtarea.value
+    fetch(gettranslation(input))
+        .then(response => response.json())
+        .then(json=>{
+            outtxt.innerText=json.contents.translated})
+        .catch(function errorHandler(error){
+            console.log("Server Error.")
+            alert("Server error!")
+        })
 })
